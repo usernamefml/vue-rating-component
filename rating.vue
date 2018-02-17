80bf30b2c61783b2daa84b22bdaca537f741f294
davidwroche <davidwroche@yahoo.co.uk>
2018-02-17 16:06:07 +0000

    first commit

---
+++ rating.vue
@@ -0,0 +1,20 @@
+<template>
+<div>
+    {{data}}
+</div>
+</template>
+
+<script>
+export default {
+    name: 'StarRating',
+    data: function() {
+        return {
+            data: "star rating"
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
