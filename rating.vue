05c5ca52b24d3911688b618df02f130f4d532004
davidwroche <davidwroche@yahoo.co.uk>
2018-02-18 13:15:49 +0000

    star update

--- rating.vue
+++ rating.vue
@@ -1,6 +1,8 @@
 <template>
 <div>
-    {{data}}
+<div class="rating">
+<span>☆</span><span>☆</span><span>☆</span><span>☆</span><span>☆</span>
+</div>
 </div>
 </template>
 
@@ -16,5 +18,18 @@ export default {
 </script>
 
 <style scoped>
-
+.rating {
+unicode-bidi: bidi-override;
+direction: rtl;
+}
+.rating > span {
+display: inline-block;
+position: relative;
+width: 1.1em;
+}
+.rating > span:hover:before,
+.rating > span:hover ~ span:before {
+ content: "\2605";
+ position: absolute;
+}
 </style>
